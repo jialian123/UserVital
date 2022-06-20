f624c04079e3d7e19b0fbc32c5d2527ae4ee206d
jialian123 <jia_li_an_2007@126.com>
2022-06-20 23:29:57 +0800

    redis commands

--- sigin_task.py
+++ sigin_task.py
@@ -10,8 +10,13 @@ if debug == 1:
     base_url = "https://123.59.87.5:12308/api.php?"
 else:
     base_url = "https://aa.tangdou.com:12308/api.php?"
-today=time.strftime("%Y%m%d",time.localtime(time.time()))
-print(today)
-exit()
+today=int(time.strftime("%Y%m%d",time.localtime(time.time())))
 account_info = account.login("13681504094","a1234567",version,base_url)
-today=time.strftime("%Y%m%d",time.localtime(time.time()))
+#签到命令
+days = 9
+with open("./redis_commands.txt",'w+',encoding="utf-8") as file_handle:
+    file_handle.writelines("签到命令:\n")
+    while days > 0:
+        file_handle.writelines("hset main:uv:sign:in:{} {} 1\n".format(account_info['uid'],today))
+        today -= 1
+        days -= 1
